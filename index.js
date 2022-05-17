5d3329d80e8d57937f6d40d42097b070d896568d
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-05-17 04:35:44 -0300

    Começando a configuração da rota get /talker

--- index.js
+++ index.js
@@ -7,9 +7,40 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const talker = [
+  {
+    name: 'Henrique Albuquerque',
+    age: 62,
+    id: 1,
+    talk: { watchedAt: '23/10/2020', rate: 5 },
+  },
+  {
+    name: 'Heloísa Albuquerque',
+    age: 67,
+    id: 2,
+    talk: { watchedAt: '23/10/2020', rate: 5 },
+  },
+  {
+    name: 'Ricardo Xavier Filho',
+    age: 33,
+    id: 3,
+    talk: { watchedAt: '23/10/2020', rate: 5 },
+  },
+  {
+    name: 'Marcos Costa',
+    age: 24,
+    id: 4,
+    talk: { watchedAt: '23/10/2020', rate: 5 },
+  },
+];
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res) => {
+  res.status(200).json(talker);
 });
 
 app.listen(PORT, () => {
