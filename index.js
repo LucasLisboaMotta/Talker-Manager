75011497377f8de2245d1234a114d6474d3f2ba3
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-05-17 06:42:31 -0300

    Complemetando função de login

--- index.js
+++ index.js
@@ -41,6 +41,16 @@ const token = () => {
   return tok.split('').filter((_, index) => index < 16).join('');
 };
 
+const valid = (a, b, c) => a >= 3 && b >= 3 && c >= 3;
+const validEmail = (email) => {
+  console.log(email);
+  const atSing = email.split('@');
+  if (!Array.isArray(atSing) || atSing.length !== 2) return false;
+  const splitPoint = atSing[1].split('.');
+  if (!Array.isArray(splitPoint) || splitPoint.length !== 2) return false;
+  return valid(atSing[0].length, splitPoint[0].length, splitPoint[1].length);
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -57,7 +67,23 @@ app.get('/talker/:id', (req, res) => {
   res.status(200).json(findTalker);
 });
 
+const valid2 = (a) => {
+ if (a === undefined || a.length === 0) return true;
+ return false;
+};
+
 app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  if (valid2(email)) { 
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  } if (!validEmail(email)) { 
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  } if (valid2(password)) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  } if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  
   const { body } = req;
   body.token = token();
   res.status(200).json(body);
